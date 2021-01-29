eaff5dbf1dd4363ed7e87570ef102eda6d855300
Scott Lurowist <scottlurowist@gmail.com>
2021-01-29 14:23:56 -0500

    ENZYME IS INSTALLED AND THERE ARE TWO TESTS FOR APP
    I installed Enzyme by issuing the following command:
    npm i --save-dev enzyme @wojtekmaj/enzyme-adapter-react-17 @types/enzyme
    
    @types.enzyme is needed to make Enzyme and Typescript play nicely together.
    
    setupTests.js was updated to support Enzyme with the Enzyme boilerplate code.
    
    I created a components folder. In that folder I created an "App" folder. Into that folder I
    moved App.tsx, App.test.tsx, and App.css.
    
    App.tsx now states "Welcome to Seinfeld. A React.js app about nothing."
    The content was modified.
    
    index.html was edited to set the title to "Seinfeld".

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
+
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('The App component must', () => {
+
+  const oneInstance: number = 1;
+  const expectedAppMessage: string = 
+    'Welcome To Seinfeld - An app about nothing.';
+  
+
+  test('contain a header', () => {
+    const componentUnderTest = shallow(<App />);
+  
+    expect(componentUnderTest.find('header').length).toBe(oneInstance);
+  });
+
+
+  test('display the text "Welcome To Seinfeld - An app about nothing."', () => {
+    const componentUnderTest = shallow(<App />);
+
+    // This is OK to do because this text is currently the only text on the page.
+    expect(componentUnderTest.text()).toBe(expectedAppMessage);
+  });
+})
+
